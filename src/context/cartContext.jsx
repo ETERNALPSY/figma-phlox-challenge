7d3a6814e20eca2a0d9303b3af05273e11298281
eternal <tr.dev.manueldominguez@gmail.com>
2023-05-17 21:35:20 -0600

    addtocart

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -8,10 +8,10 @@ export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
    const [cart, setCart] = useState([]);
-   
+   console.log(cart)
 
    const addToCart = (newItem) => {
-      setCart(...cart, newItem );
+      setCart([...cart, newItem ]);
    };
 
    const deleteToCart = (id) => {
