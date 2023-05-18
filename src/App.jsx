7cd6e567e027674df03ec81d373327a6352a1773
Mario Martinez <mariomartinezmesm@gmail.com>
2023-05-17 20:53:41 -0600

    fix:errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,31 @@ import CategoryCards from './components/categoryCards/categoryCards'
 import BestProducts from './components/bestProducts/bestProducts'
 import './App.css'
 import DetailProduct from './pages/detailProduct/detailProduct'
+import Tags from './components/tags/tags'
 import Navbar from './components/nav/nav'
 import Main from './components/main/main'
+
 
 
 function App() {
 
    return (
       <>
-         <Navbar/>
-         <Main/>
-         <CategoryCards />
+         <Navbar />
          <Routes>
-            <Route path='/' element={<BestProducts />} > 
+            <Route path='/' element={
+               <>
+                  <Main />
+                  <CategoryCards />
+                  <Tags />
+                  <BestProducts />
+               </>
+            }>
+
                <Route path='/product/:id' element={<DetailProduct />} />
             </Route>
 
-            <Route path='*' element={<Link className='' to="/">Volver</Link>} /> //default
+            <Route path='*' element={<Link className='' to="/">Volver</Link>} />
 
          </Routes>
 
